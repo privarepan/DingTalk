f7e97465022a36d8c4335b1c6c472ae466808c3a
潘章一 <panzhangyi@squrab.com>
2020-05-09 11:12:34 +0800

    first

--- config/ding.php
+++ config/ding.php
@@ -1,7 +1,20 @@
 <?php
 
 return [
-    'app_key' => env('DT_KEY'),
-    'app_secret' => env('DT_SECRET','SECdb86f33751ce3c892e242a7c2ca6a5595898b12151887e87d70fda68aa7ed6c3'),
-    'agent_id' => env('DT_AGENT_ID'),
+    'robot' => [
+        'default' => 'think007',
+        'think007' => [
+            'access_token' => env('THINK007_TOKEN',''),
+            'secret' => env('THINK007_SECRET','')
+        ]
+    ],
+    'work-notice' => [
+        'appkey' => env('DT_KEY'),
+        'appsecret' => env('DT_SECRET'),
+        'agent_id' => env('DT_AGENT_ID'),
+    ],
+
+    'http' => [
+        'timeout' => env('DT_TIMEOUT',2),
+    ],
 ];
